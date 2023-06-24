9b8d6af5cacd5142032b67883650849cb33c7bac
Folafunmi <folafunmimustapha@gmail.com>
2023-06-24 22:31:43 +0100

    feat: add modal to leave

--- src/app/room/page.tsx
+++ src/app/room/page.tsx
@@ -2,15 +2,22 @@
 
 import { Nav } from "@/components/nav";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { useToast } from "@/components/ui/use-toast";
 import Balancer from "react-wrap-balancer";
-import Link from "next/link";
-import { DoorOpen, LogIn, LogOut } from "lucide-react";
+import { Copy, LogOut } from "lucide-react";
 import * as Ably from "ably/promises";
 import { configureAbly, useChannel, usePresence } from "@ably-labs/react-hooks";
 import * as React from "react";
 import { useRouter } from "next/navigation";
+import { copyToClipboard, getCurrentURL } from "@/lib/utils";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export default function Room({
   searchParams,
@@ -18,6 +25,7 @@ export default function Room({
   params: { slug: string };
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
+  const [show, setShow] = React.useState(false);
   const router = useRouter();
 
   const { toast } = useToast();
@@ -25,6 +33,12 @@ export default function Room({
   const roomId = searchParams?.id;
   const roomName = searchParams?.name;
   const userName = searchParams?.user;
+  const currentUrl = getCurrentURL();
+
+  const joinRoute = `/join?id=${roomId}&name=${roomName}`;
+  const roomRoute = `/room?id=${roomId}&name=${roomName}&user=${userName}`;
+  const roomUrl = `${currentUrl}${roomRoute}`;
+  const joinUrl = `${currentUrl}${joinRoute}`;
 
   React.useEffect(() => {
     if (!(userName && roomId && roomName)) {
@@ -80,32 +94,84 @@ export default function Room({
         <div className="w-full max-w-xl text-6xl my-2 font-bold text-center mx-auto">
           <Balancer>{roomName}</Balancer>
         </div>
-        <Button
-          onClick={() => {
-            handleLeaveClick();
-          }}
-          variant={"destructive"}
-          className="space-x-4"
-        >
-          <LogOut height={16} width={16} className="mx-2" />I want to go home!
-        </Button>
-        <Button
-          onClick={() => {
-            channel.publish("voted", { text: 1 });
-          }}
-        >
-          1
-        </Button>
-        <ul>
-          {presenceData.map((msg, index) => (
-            <li key={index}>{msg.clientId}</li>
-          ))}
-        </ul>
-        <ul>
-          {messages.map((msg, index) => (
-            <li key={index}>{msg.data}</li>
-          ))}
-        </ul>
+        <div className="w-full flex justify-center items-center flex-wrap gap-2">
+          <Button
+            variant={"outline"}
+            type="button"
+            className="space-x-1"
+            onClick={() => {
+              copyToClipboard(joinUrl);
+              toast({ description: "Copied room link!", duration: 5000 });
+            }}
+          >
+            <Copy height={16} width={16} />
+          </Button>
+
+          <Dialog open={show} onOpenChange={setShow}>
+            <DialogTrigger asChild>
+              <Button
+                onClick={() => {
+                  setShow(true);
+                }}
+                variant={"destructive"}
+                className=""
+              >
+                <LogOut height={16} width={16} className="" />
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Leave Room</DialogTitle>
+              </DialogHeader>
+              <div className="flex flex-col justify-start items-start gap-4 py-4">
+                <div className="flexjustify-start items-center space-x-2">
+                  <p className="">Are you sure you want to leave?</p>{" "}
+                </div>
+              </div>
+              <DialogFooter className="flex w-full">
+                <Button
+                  variant={"secondary"}
+                  type="button"
+                  className="flex-1 space-x-1"
+                  onClick={() => {
+                    setShow(false);
+                  }}
+                >
+                  <span>No, I want to stay!</span>
+                </Button>
+                <Button
+                  variant={"destructive"}
+                  type="button"
+                  className="flex-1 space-x-1"
+                  onClick={() => {
+                    handleLeaveClick();
+                  }}
+                >
+                  <span>Yes, I want to leave!</span>
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
+        <div className="w-full grid auto-rows-auto justify-center align-center">
+          <Button
+            onClick={() => {
+              channel.publish("voted", { text: 1 });
+            }}
+          >
+            1
+          </Button>
+          <ul>
+            {presenceData.map((msg, index) => (
+              <li key={index}>{msg.clientId}</li>
+            ))}
+          </ul>
+          <ul>
+            {messages.map((msg, index) => (
+              <li key={index}>{msg.data}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </main>
   );
