bdeb63741135f8b12085c00584a353ea9a8df0f9
Folafunmi <folafunmimustapha@gmail.com>
2023-07-11 10:21:51 +0100

    feat: add github logo

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,7 +1,16 @@
+import { Github } from "lucide-react";
+
 const Footer = () => {
   return (
-    <div className="text-sm top-[100vh] sticky mb-2">
-      © {new Date().getFullYear()} Sprintblitz
+    <div className="text-sm top-[100vh] flex  justify-center items-center gap-1 sticky mb-2">
+      © {new Date().getFullYear()} Sprintblitz.
+      <a
+        href="https://github.com/folafunmi-db"
+        rel="noreferrer noopener"
+        className="flex justify-center items-center gap-1 text-sm"
+      >
+        <Github width={14} height={14} />
+      </a>
     </div>
   );
 };
