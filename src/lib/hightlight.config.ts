b81994756f9586d2c3f2d404a70abf8a3228413a
Folafunmi <folafunmimustapha@gmail.com>
2023-07-26 11:57:18 +0100

    fix: type

--- src/lib/hightlight.config.ts
+++ src/lib/hightlight.config.ts
@@ -2,5 +2,5 @@ import { Highlight } from "@highlight-run/next";
 import { CONSTANTS } from "./constants";
 
 export const withHighlight = Highlight({
-  projectID: CONSTANTS.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID,
+  projectID: CONSTANTS.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID || "",
 });
