f7b3343a1484d56f5e72df9c4abe9983c3bbb495
Folafunmi <folafunmimustapha@gmail.com>
2023-11-20 15:56:22 +0100

    chore: add handlers

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,14 @@
 import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export default {
-  schema: "./src/db/schema.ts",
-  out: "./drizzle",
+  schema: "./src/db/app/schema.ts",
+  out: "./drizzle/migrations",
   driver: "turso",
+  strict: true,
+  dbCredentials: {
+    url: process.env.DATABASE_URL as string,
+    authToken: process.env.DATABASE_AUTH_TOKEN as string,
+  },
 } satisfies Config;
---
+++ src/app/retro/get-room-posts/route.ts
@@ -0,0 +1,15 @@
+import { db } from "@/db/migrate";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const id = searchParams.get("id");
+
+  const res = await db.query.posts.findMany({
+    where: (posts, { eq }) => eq(posts.roomId, Number(id)),
+  });
+
+  const data = res;
+
+  return NextResponse.json({ data });
+}
